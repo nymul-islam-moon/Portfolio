56d903772aaa77a17ad4c40a2e39ef35b4b3fa41
Nymul Islam Moon <towkir1997islam@gmail.com>
2022-10-19 20:49:22 +0600

    Home
    home route Created

--- portfolio/routes/web.php
+++ portfolio/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(HomeController::class)->prefix('home')->group(function () {
+    Route::get('/index', 'index')->name('admin.home.index');
 });
